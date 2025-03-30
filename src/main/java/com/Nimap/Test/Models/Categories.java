c0882086e9dd4f0a09791a3f0ee975e075dac0b8
Rohan Patil <rohan.codecraft@gmail.com>
2025-03-30 15:43:16 +0530

    Model class setup

--- src/main/java/com/Nimap/Test/Models/Categories.java
+++ src/main/java/com/Nimap/Test/Models/Categories.java
@@ -1,5 +1,55 @@
 package com.Nimap.Test.Models;
 
-public class Categories {
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@Entity  // Marks this class as a JPA entity (database table)
+@AllArgsConstructor // Created a constructor with all fields
+@NoArgsConstructor   // Created a default constructor
+public class Categories {
+	
+    @Id  // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increment primary key
+    private int cid;  // Category ID
+    
+    private String cname;  // Category name
+    
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)  
+    // One category can have multiple products
+    private List<Products> products;  
+
+    // Getter and Setter for all feilds
+    public int getCid() {
+        return cid;
+    }
+    
+    public void setCid(int cid) {
+        this.cid = cid;
+    }
+
+   
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    
+    public String getCname() {
+        return cname;
+    }
+
+    
+    public void setProducts(List<Products> products) {
+        this.products = products;
+    }
+    
+    public List<Products> getProducts() {
+        return products;
+    }
 }
--- src/main/java/com/Nimap/Test/Models/Products.java
+++ src/main/java/com/Nimap/Test/Models/Products.java
@@ -1,5 +1,67 @@
 package com.Nimap.Test.Models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity  // Marks this class as a JPA entity (database table)
+@AllArgsConstructor  // Created a constructor with all fields
+@NoArgsConstructor   // Created a default constructor
 public class Products {
 
+    @Id  // Primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // Auto-increment primary key
+    private int pid;  // Product ID
+
+    private String pname;  // Product name
+    private Double price;  // Product price
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)  
+    @JoinColumn(name = "category_id", referencedColumnName = "cid")  
+    // Many products belong to one category (foreign key relation)
+    private Categories category;  
+
+    // Getter and Setter for All Feilds
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+
+    public int getPid() {
+        return pid;
+    }
+
+   
+    public void setPname(String pname) {
+        this.pname = pname;
+    }
+
+    public String getPname() {
+        return pname;
+    }
+
+   
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    
+    public Double getPrice() {
+        return price;
+    }
+
+   
+    public void setCategory(Categories category) {
+        this.category = category;
+    }
+
+    public Categories getCategories() {
+        return category;
+    }
 }
